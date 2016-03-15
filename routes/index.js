a319072f13abfbfa0dee954c2f2be1860a965115
Todd Hancock <thancock20@hotmail.com>
2016-03-15 12:59:25 -0500

    Work on /imagesearch route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var Url = require('../models/url');
+var search = require('../models/search');
 
 var router = express.Router();
 
@@ -11,33 +11,28 @@ router.get('/', function(req, res) {
   });
 });
 
-router.get('/new/*', function(req, res) {
-  var original_url = (req.originalUrl).replace('/new/', '');
-  if (original_url.search(/^https*:\/\/.*\..*$/) === -1) {
+router.get('/imagesearch/:term', function(req, res) {
+  console.log(req.params.term);
+  console.log(req.query.offset);
+  /* var url = new Url();
+  url.original_url = original_url;
+  url.save(function(err) {
+    if (err) res.send(err);
+    var hostname = req.hostname === 'localhost' ? 'http://localhost:' + port : req.protocol + '://' + req.hostname;
     res.json({
-      error: 'invalid URL'
+      original_url: original_url,
+      short_url: hostname + '/' + url.id
     });
-  } else {
-    var url = new Url();
-    url.original_url = original_url;
-    url.save(function(err) {
-      if(err) res.send(err);
-      var hostname = req.hostname === 'localhost' ? 'http://localhost:' + port : req.protocol + '://' + req.hostname;
-      res.json({
-        original_url: original_url,
-        short_url: hostname + '/' + url.id
-      });
-    });
-  }
+  }); */
 });
 
-router.get('/:url_id', function(req, res) {
+/* router.get('/:url_id', function(req, res) {
   Url.findById(req.params.url_id, function(err, url) {
     if (err)
       res.send(err);
     if (url)
       res.redirect(url.original_url);
   });
-});
+}); */
 
 module.exports = router;
